03ce81d4d68ce307b6ef54184dc5c8ae18ebccfc
Quan Dao <daominhquan9909@gmail.com>
2018-11-26 17:28:10 +0100

    add functioning for NN

--- neuralNetClass.py
+++ neuralNetClass.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class NeuralNet(object):
 	"""docstring for NeuralNet"""
-	def __init__(self, num_input_layer, num_output_layer, num_hidden_layers, list_num_neurons_per_hidden, learning_rate=0.01):
+	def __init__(self, num_input_layer, num_output_layer, num_hidden_layers, list_num_neurons_per_hidden, batch_size=1, learning_rate=0.01):
 		self.n_in = num_input_layer  # number of neurons on the input layer
 		self.n_out = num_output_layer  # number of neurons on the output layer
 		self.depth = num_hidden_layers + 2  # total number of layers in the model
@@ -26,6 +26,7 @@ class NeuralNet(object):
 		# storing dictionary
 		self.weighted_input_dict = {}  # store forward propagation result
 		self.modified_error_dict ={}  # store backward propagation result
+		self.net_input = np.zeros((self.n_in, batch_size))
 
 
 	def layerAct(self, x):
@@ -36,10 +37,15 @@ class NeuralNet(object):
 		return self.layerAct(x) * (1 - self.layerAct(x))
 
 
-	def netForwardProp(self, net_input):
+	def netFedInput(self, _net_input):
+		assert _net_input.shape[0] == self.n_in
+		self.net_input = _net_input
+
+
+	def netForwardProp(self):
 		for key in self.weights_dict.keys():
 			if key == 1:
-				z = np.matmul(self.weights_dict[key], net_input) + self.biases_dict[key]
+				z = np.matmul(self.weights_dict[key], self.net_input) + self.biases_dict[key]
 			else:
 				z = np.matmul(self.weights_dict[key], self.layerAct(self.weighted_input_dict[key - 1])) + self.biases_dict[key]
 			# store z
@@ -48,13 +54,38 @@ class NeuralNet(object):
 		return self.layerAct(self.weighted_input_dict[self.depth - 1])
 
 
-	def netBackProp(self, net_input, true_labels):
-		self.modified_error_dict[self.depth - 1] = self.netForwardProp(net_input) - true_labels
+	def netBackProp(self, true_labels):
+		self.modified_error_dict[self.depth - 1] = self.netForwardProp() - true_labels
 		for i in xrange(self.depth - 2, 0, -1):
 			self.modified_error_dict[i] = np.matmul(self.weights_dict[i + 1].transpose(), self.modified_error_dict[i + 1]) * self.layerDiffAct(self.weighted_input_dict[i])
 
-		
 
+	def netWeightUpdate(self):
+		alpha = self.learning_rate
+		m = self.net_input.shape[1]
+		vec_one = np.zeros((1, m)) + 1
+		for i in range(1, self.depth):
+			# update weight matrixes
+			if i == 1:
+				self.weights_dict[i] -= (alpha / m) * np.matmul(self.modified_error_dict[i], self.net_input.transpose())
+			else:
+				self.weights_dict[i] -= (alpha / m) * np.matmul(self.modified_error_dict[i], self.layerAct(self.weighted_input_dict[i - 1]).transpose())
+			# update biases
+			self.biases_dict[i] -= (alpha / m) * np.matmul(self.modified_error_dict[i], vec_one.transpose())
+
+
+	def netEval(self, test_data, test_label):
+		self.netFedInput(test_data)
+		predict = self.netForwardProp()
+		# convert predict to binary
+		predict = predict >= np.amax(predict, axis=0)
+		# count the wrong prediction
+		diff_matrix = predict.astype(int) - test_label
+		num_wrong = np.sum((diff_matrix == -1).astype(int))
+
+		return 1 - num_wrong / test_label.shape[1]
+
+		
 if __name__ == '__main__':
 	test_ann = NeuralNet(3, 2, 1, [2])
 	for key in test_ann.weights_dict.keys():
@@ -64,8 +95,11 @@ if __name__ == '__main__':
 		print(test_ann.biases_dict[key])
 		print("-------------------")
 	
-	net_input = np.array([[1, 1, 1]])
-	true_labels = np.array([[1, 0]])
-	test_ann.netBackProp(net_input.transpose(), true_labels.transpose())
-	print("Output: ")
-	print(test_ann.modified_error_dict)
+	net_input = np.eye(3)
+	true_labels = np.array([[1, 0, 0], [0, 1, 1]])
+	test_ann.netFedInput(net_input)
+	test_ann.netBackProp(true_labels)
+	test_ann.netWeightUpdate()
+	correct = test_ann.netEval(net_input, true_labels)
+
+	print("Output: ", correct * 100)
